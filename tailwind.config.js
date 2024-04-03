3ed7b12ca7e3afe3b191fafa105f5ef60388a80a
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2024-04-03 22:29:37 +0700

    Fixed conflict style if configure tailwind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ import preset from './vendor/filament/support/tailwind.config.preset'
  
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [preset],
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
