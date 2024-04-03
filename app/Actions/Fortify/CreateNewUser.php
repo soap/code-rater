c4c3e3e465c309c3c6d1b6232b8e393e82f51ba3
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2024-04-03 17:59:22 +0700

    Add default role for user while creating

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -27,11 +27,15 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'first_name' => $input['first_name'],
             'last_name' => $input['last_name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $user->assignRole('Customer');
+        
+        return $user;
     }
 }
